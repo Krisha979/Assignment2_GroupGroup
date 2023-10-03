e0438b951125af81773020ae57fe38d662c73af8
Mahat <mahatk@student.douglascollege.ca>
2023-10-03 16:36:10 -0700

    chore: Project folders

---
+++ src/main/java/com/CSIS3275/Assignment2GroupGroupApplication.java
@@ -0,0 +1,13 @@
+package com.CSIS3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2GroupGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment2GroupGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CSIS3275/Assignment2GroupGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSIS3275;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2GroupGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
