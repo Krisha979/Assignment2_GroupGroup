7d766ae4f3c27e5417debcdd9c648e5e5b03e54d
Mahat <mahatk@student.douglascollege.ca>
2023-10-03 16:55:16 -0700

    feat: Model folder Added

---
+++ src/main/java/com/CSIS3275/Model/Group.java
@@ -0,0 +1,7 @@
+package com.CSIS3275.Model;
+
+public class Group {
+	public String GroupName;
+	public int NumOfMemb;
+	public String Desc;
+}
---
+++ src/main/java/com/CSIS3275/Model/GroupMember.java
@@ -0,0 +1,8 @@
+package com.CSIS3275.Model;
+
+public class GroupMember {
+
+	public String Name;
+	public String Email;
+	public int StudentId;
+}
