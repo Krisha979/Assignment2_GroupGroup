b17b1ca98dba98daaf25587e84817dea9bbe90c0
aesop011094 <leungk24@student.douglascollege.ca>
2023-10-03 21:21:11 -0700

    feat: edit controller  to support group details

--- src/main/java/com/CSIS3275/controller/TeamMemberController_GroupGroup.java
+++ src/main/java/com/CSIS3275/controller/TeamMemberController_GroupGroup.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.CSIS3275.Model.Group;
 import com.CSIS3275.Model.GroupMember;
 
 @Controller
@@ -11,6 +12,10 @@ public class TeamMemberController_GroupGroup {
 	
 	@GetMapping("/")
 	public String getTeamMembers(Model model) {
+		Group group = new Group("GroupGroup", 3, "Collaborative team working on Csis3275");
+		
+		model.addAttribute("groupDetails",group);
+		
 		return "team-member-list";
 	}
 	
